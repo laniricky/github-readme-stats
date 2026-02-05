14884862ae1728cdf44cdc3638b15d2e18342731
Fredrick Lani <fredricklani@gmail.com>
2026-02-05 09:25:30 +0300

    feat: enhance docs to A+++ standard and add laniricky theme

---
+++ themes/laniricky.js
@@ -0,0 +1,8 @@
+// Laniricky personalized theme
+module.exports = {
+  title_color: '00e676', // Bright Green
+  icon_color: '00e676',
+  text_color: 'ffffff',
+  bg_color: '0d1117', // Dark GitHub
+  url_color: '00e676',
+};
